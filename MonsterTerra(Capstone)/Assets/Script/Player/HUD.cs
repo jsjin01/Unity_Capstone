728d742a4dd54d3485c7c2ef2b12d1b90a77c54f
ok6334kr <ok6334kr@naver.com>
2024-05-24 18:05:03 +0900

    UI 작업
    Exp , Hp, Mp UI 작업 완료

--- MonsterTerra(Capstone)/Assets/Script/Player/HUD.cs
+++ MonsterTerra(Capstone)/Assets/Script/Player/HUD.cs
@@ -5,23 +5,27 @@ using UnityEngine.UI;
 
 public class HUD : MonoBehaviour
 {
+    public InfoType type;
+    Text myText;
+    Slider mySlider;
+    Spawner spawner;
+
     public enum InfoType
     {
         Exp,
         Level,
         Kill,
         Time,
-        Health
+        Hp,
+        Mp,
+        Stage,
     }
-
-    public InfoType type;
-    Text myText;
-    Slider mySlider;
 
     void Awake()
     {
         myText = GetComponent<Text>();
         mySlider = GetComponent<Slider>();
+        spawner = FindObjectOfType<Spawner>();
     }
 
     void LateUpdate()
@@ -32,6 +36,19 @@ public class HUD : MonoBehaviour
                 float curExp = GamePlayerManager.i.exp;
                 float maxExp = GamePlayerManager.i.maxExp[GamePlayerManager.i.lv];
                 mySlider.value = curExp / maxExp;
+                break;
+            case InfoType.Hp:
+                float curHp = GamePlayerManager.i.hp;
+                float maxHp = GamePlayerManager.i.MaxHp;
+                mySlider.value = curHp / maxHp;
+                break;
+            case InfoType.Mp:
+                float curMp = GamePlayerManager.i.mp;
+                float maxMp = GamePlayerManager.i.MaxMp;
+                mySlider.value = curMp / maxMp;
+                break;
+            case InfoType.Stage:
+                myText.text = string.Format("Stage {0}", spawner.level + 1);
                 break;
         }
     }
