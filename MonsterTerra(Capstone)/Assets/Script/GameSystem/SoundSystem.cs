acdd601315b9cafa6428c99c6ca9dd5af03c4d4c
Jeon sangjin <133385686+jsjin01@users.noreply.github.com>
2024-06-02 22:10:08 +0900

    .

--- MonsterTerra(Capstone)/Assets/Script/GameSystem/SoundSystem.cs
+++ MonsterTerra(Capstone)/Assets/Script/GameSystem/SoundSystem.cs
@@ -1,18 +1,67 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class SoundSystem : MonoBehaviour
+public class SoundManger : MonoBehaviour
 {
+    public static SoundManger i;
+    GameObject baseobj;
+    [SerializeField] AudioSource Bgm;
+    [SerializeField] AudioClip[] clipList;
+
+    private void Awake()
+    {
+        i = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        baseobj = transform.GetChild(0).gameObject;
+        Bgm = GetComponent<AudioSource>();
+    }
+    private void Update()
+    {
+        Bgm.volume = PlayerPrefs.GetFloat("Volume_Back", 50);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlaySound(int id, bool _loop = false)          //사운드 타입과 타입에 따른 인덱스 
     {
-        
+        AudioSource s = null;
+
+        //사운드가 플레이 중이지 않은 오브젝트가 있다면
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<AudioSource>().isPlaying == false)
+            {
+                s = transform.GetChild(i).GetComponent<AudioSource>();
+                break;
+            }
+        }
+
+        if (s == null)
+        {
+            GameObject temp = Instantiate(baseobj, transform); //오브젝트 하나 생성
+            s = temp.GetComponent<AudioSource>();
+        }
+
+        s.volume = PlayerPrefs.GetFloat("Volume_Effect", 50);
+        s.clip = clipList[id];                  //사운드 클립 변경
+        s.loop = _loop;                         //루프 상태 변경
+        s.Play();                               //사운드 플레이
+    }
+
+
+    public void StopSound(int id)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).GetComponent<AudioSource>().clip == clipList[id] &&
+                transform.GetChild(i).GetComponent<AudioSource>().isPlaying)
+            {
+                transform.GetChild(i).GetComponent<AudioSource>().Stop();
+                break;
+            }
+        }
     }
 }
